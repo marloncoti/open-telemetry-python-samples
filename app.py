edfd54ec59b0aefc05e5420a3a2021b9784a830c
Marlon Coti <marlon.coti@mypeopleapps.com>
2025-02-26 16:12:26 -0600

    added basic FAST API Service

--- app.py
+++ app.py
@@ -4,18 +4,15 @@ import requests
 # Initialize FastAPI app
 app = FastAPI()
 
-# 🔹 **GET Endpoint: Simple Greeting**
 @app.get("/hello")
 async def hello():
     return {"message": "Hello, world!"}
 
-# 🔹 **POST Endpoint: Addition**
 @app.get("/add")
 async def add_numbers(a: int, b: int):
     result = a + b
     return {"operation": f"{a} + {b}", "result": result}
 
-# 🔹 **GET Endpoint: Call External API**
 @app.get("/search_name")
 async def call_external_api(name: str = 'Kevin'):
     response = requests.get(f"https://api.snationalize.io?name={name}")  
