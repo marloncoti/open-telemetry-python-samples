0af9b5f4f2d45bce31e5677b6c26076b54f5bbb2
Marlon Coti <marlon.coti@mypeopleapps.com>
2025-02-26 16:11:36 -0600

    added autoinstrumentation

---
+++ README.md
@@ -0,0 +1,12 @@
+# autoinstrumentación
+install open telemetry zero-code packages.
+
+```bash
+python -m pip install opentelemetry-instrumentation \
+                      opentelemetry-distro \
+                      opentelemetry-exporter-otlp
+```
+
+
+install instrumentation libraries   
+`opentelemetry-bootstrap --action=install`
--- app.py
+++ app.py
@@ -1,24 +1,22 @@
 from fastapi import FastAPI
 import requests
 
-# Initialize FastAPI app
 app = FastAPI()
 
-# 🔹 **GET Endpoint: Simple Greeting**
+
 @app.get("/hello")
 async def hello():
     return {"message": "Hello, world!"}
 
-# 🔹 **POST Endpoint: Addition**
 @app.get("/add")
 async def add_numbers(a: int, b: int):
     result = a + b
     return {"operation": f"{a} + {b}", "result": result}
+    
 
-# 🔹 **GET Endpoint: Call External API**
 @app.get("/search_name")
 async def call_external_api(name: str = 'Kevin'):
-    response = requests.get(f"https://api.snationalize.io?name={name}")  
+    response = requests.get(f"https://api.nationalize.io?name={name}")  
     if response.status_code == 200:
         return response.json()
     return {"error": "Failed to fetch data from external API"}
